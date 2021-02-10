dc0c5c33552c188d1526a8646c968479a0337075
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-09 17:08:39 -0800

    commit tree 89c651abafd3563c658dfc597bb032644bb9e94e

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Percy Jackson and the Olympians", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "good vs evil");
+
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Rick Riordin");
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "The Lightning Theif", 2002, 2), (5, "The Sea of Monsters", 2003, 2), (6, "The Titan's Curse", 2005, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Percy Jackson", "Aw, man", "demigod", 2), (6, "Annabeth Chase", "Hey, Seaweed brains", "demigod", 2), (7, "Grover Underwood", "mmm tin cans", "satyr", 2), (8, "Blackjack", "yo, duuuude", "pegasus", 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
